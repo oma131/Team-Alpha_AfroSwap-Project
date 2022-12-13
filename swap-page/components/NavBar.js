8e9dfc83bdaa5efca217180f3a0dce568b89ea5b
oma131 <anosikeihuoma@gmail.com>
2022-12-13 14:45:45 +0100

    hold comit

--- swap-page/components/NavBar.js
+++ swap-page/components/NavBar.js
@@ -1,82 +1,72 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { useState } from 'react';
 
 export const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
 
   return (
-    <header aria-label="Site Header" className="bg-midnight text-white">
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 items-center justify-between">
-            <div className="flex-1 md:flex md:items-center md:gap-12">
-                <a className="block text-teal-600" href="/">
-                <span className="sr-only">Home</span>
-                <img src='./fullLogo.svg' alt="Afroswap Logo" className='h-10' />
-                </a>
-            </div>
-
-            <div className="md:flex md:items-center md:gap-12">
-                <nav aria-label="Site Nav" className="hidden md:block">
-                <ul className="nav-list flex items-center gap-6 text-sm">
-                    <li>
-                    <a
-                        className="text-gray-500 transition hover:text-gray-500/75"
-                        href="/"
-                    >
+    <>
+      <nav className='sticky top-0 flex items-center flex-wrap bg-midnight px-12 py-2 '>
+        <Link href='/'>
+          <div className='inline-flex items-center p-2 mr-2 '>
+            <img src='./fullLogo.svg' alt='Afroswap Main logo' />
+          </div>
+        </Link>
+        <button
+          className=' block p-3 hover:bg-purple rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+          onClick={handleClick}
+        >
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M4 6h16M4 12h16M4 18h16'
+            />
+          </svg>
+        </button>
+        {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+        <div
+          className={`${
+            active ? '' : 'hidden'
+          }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
+            <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+                <Link href='/'>
+                    <div className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center  hover:text-purple '>
                         Discover
-                    </a>
-                    </li>
-
-                    <li>
-                    <a
-                        className="text-gray-500 transition hover:text-gray-500/75"
-                        href="/"
-                    >
-                        About Us
-                    </a>
-                    </li>
-
-                    <li>
-                    <Link
-                        className="text-gray-500 transition hover:text-gray-500/75"
-                        href="/learn"
-                    >
-                        Learn More
-                    </Link>
-                    </li>
-                </ul>
-                </nav>
-
-                <div className="flex items-center gap-4">
-                <div className="sm:flex sm:gap-4">
-                    <div className="rounded-md bg-gradient-to-r from-purple via-fuchsia to-fuchsia text-white px-5 py-3.5 text-sm font-medium shadow">
-                        <Link href="/swap">Swap Token</Link>
                     </div>
-                </div>
-
-                <div className="block md:hidden">
-                    <button
-                    className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
-                    >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        stroke-width="2"
-                    >
-                        <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4 6h16M4 12h16M4 18h16"
-                        />
-                    </svg>
-                    </button>
-                </div>
-                </div>
-            </div>
+                </Link>
+                <Link href='/'>
+                    <div className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center  hover:text-purple'>
+                        About Us
+                    </div>
+                </Link>
+                <Link href='/learn'>
+                    <div className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center  hover:text-purple'>
+                        Learn more
+                    </div>
+                </Link>
             </div>
         </div>
-    </header>
-
+        <Link href="/swap">
+            <div
+            class="inline-block rounded bg-gradient-to-r from-purple via-fuchsia to-fuchsia px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:bg-indigo-500"
+            >
+            Swap Token
+            </div>
+        </Link>
+      </nav>
+    </>
   );
 };
